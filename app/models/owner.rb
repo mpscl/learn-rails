6da152114540c593dc69b54448a62c69f388e4c8
Marco Pascale <r4ncor@gmail.com>
2014-02-14 09:37:03 -0430

    cosas nuevas pagina dinamica yy todo

---
+++ app/models/owner.rb
@@ -0,0 +1,21 @@
+class Owner
+	def name
+		name = 'Foobar Kadigan'
+	end
+
+	def birthdate
+		birthdate = Date.new(1982, 11, 05)
+	end
+
+	def countdown
+		Rails.logger.debug 'DEBUG: entering Owner countdown method'
+		Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
+		today = Date.today
+		birthday = Date.new(today.year, birthdate.month, birthdate.day)
+		if birthday > today
+			countdown = (birthday - today).to_i
+		else
+			countdown = (birthday.next_year - today).to_i
+		end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ LearnRails::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  root to: 'visitors#new'
   # Example resource route with options:
   #   resources :products do
   #     member do
